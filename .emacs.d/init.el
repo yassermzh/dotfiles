19b93dfe6eceda1974292c4fb1cd182885c741ef
Yasser Mohammadzadeh <yassermzh@gmail.com>
2014-08-16 10:26:41 +0430

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -285,8 +285,17 @@
 (autoload 'tern-mode "tern.el" nil t)
 (add-hook 'js3-mode-hook (lambda () (tern-mode t)))
 (add-hook 'js3-mode-hook (lambda () (auto-complete-mode t)))
+(add-hook 'js3-mode-hook (lambda () (flyspell-mode t)))
 (eval-after-load 'tern
   '(progn
      (require 'tern-auto-complete)
      (tern-ac-setup)))
+     
+     
+;; ------------------------
+;; -- my stuff: spell check with camel case support
+;; ------------------------
+;; install aspell
+(load "spell-check")
+;; install flyspell
 (ac-flyspell-workaround)
