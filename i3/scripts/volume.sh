e2a3843e06d619f95e1c180d08fd93f06d0b5b28
yasser <yassermzh@gmail.com>
2022-07-23 21:31:20 +0200

    add volume with notification

---
+++ i3/scripts/volume.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# You can call this script like this:
+# $./volume.sh up
+# $./volume.sh down
+# $./volume.sh mute
+
+function get_volume {
+  pacmd list-sinks|grep -A 15 '* index'| awk '/volume: front/{ print $5 }' | sed 's/[%|,]//g' 
+}
+
+function is_mute {
+  pacmd list-sinks | grep -A 15 '* index'| awk '/muted: /{print $2}'
+}
+
+function send_notification {
+    volume=`get_volume`
+    # Make the bar with the special character ─ (it's not dash -)
+    # https://en.wikipedia.org/wiki/Box-drawing_character
+    bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')
+    # Send the notification
+    dunstify -i audio-volume-muted-blocking -t 800 -r 2593 -u normal "$volume% $bar"
+}
+
+case $1 in
+    up)
+    # Set the volume on (if muted it was)
+    pactl set-sink-mute @DEFAULT_SINK@ off > /dev/null
+    # Up the volume (+ 5%)
+    volume=`get_volume`
+    volume=$((volume + 2 > 100 ? 100 : volume + 2))
+    pactl set-sink-volume @DEFAULT_SINK@ "$volume%" > /dev/null
+    send_notification
+    ;;
+    down)
+    pactl set-sink-mute @DEFAULT_SINK@ off > /dev/null
+    volume=`get_volume`
+    volume=$((volume - 5 > 0 ? volume - 5 : 0))
+    pactl set-sink-volume @DEFAULT_SINK@ "$volume%" > /dev/null
+    send_notification
+    ;;
+    mute)
+    # Toggle mute
+    pactl set-sink-mute @DEFAULT_SINK@ toggle > /dev/null
+    if [ `is_mute` == "yes" ]; then
+        dunstify -i audio-volume-muted -t 800 -r 2593 -u normal "Mute"
+    else
+        send_notification
+    fi
+    ;;
+esac
