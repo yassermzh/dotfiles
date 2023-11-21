ab0c9cd3cfd3601b30412e5a6ec5039db44b1670
Yasser Zadeh <yamoh@spreadgroup.com>
2023-11-21 12:43:13 +0100

    add markdown

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -72,7 +72,17 @@ return {
             -- add any options here
         },
         lazy = false,
-    }
+    },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+    },
 
     -- images
     {
