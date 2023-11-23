488230324892e1bf9702d20c9545af5b44d398f9
Yasser Zadeh <yamoh@spreadgroup.com>
2023-11-23 10:17:47 +0100

    set dark mode as default

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,9 +1,9 @@
 require('rose-pine').setup({
-    disable_background = true
+    disable_background = false
 })
 
-function ColorMyPencils(color) 
-	color = color or "rose-pine"
+function ColorMyPencils(color)
+	color = color or "rose-pine-moon"
 	vim.cmd.colorscheme(color)
 
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
