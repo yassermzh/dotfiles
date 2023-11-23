796806b74287dff5a47079d409ae2a17d062fe77
Yasser Zadeh <yamoh@spreadgroup.com>
2023-11-23 19:22:41 +0100

    add organize imports command

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -81,3 +81,24 @@ lsp.setup()
 vim.diagnostic.config({
     virtual_text = true
 })
+
+
+local function organize_imports()
+    local params = {
+        command = "_typescript.organizeImports",
+        arguments = { vim.api.nvim_buf_get_name(0) },
+        title = ""
+    }
+    vim.lsp.buf.execute_command(params)
+end
+
+require("lspconfig").tsserver.setup {
+    -- on_attach = on_attach,
+    -- capabilities = capabilities,
+    commands = {
+        OrganizeImports = {
+            organize_imports,
+            description = "Organize Imports"
+        }
+    }
+}
