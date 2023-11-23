82764df54821a4515455f5766da50807a66a5203
Yasser Zadeh <yamoh@spreadgroup.com>
2023-11-23 19:48:51 +0100

    disable bg color to none

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -1,13 +1,15 @@
 require('rose-pine').setup({
   disable_background = false,
+  --- @usage 'auto'|'main'|'moon'|'dawn'
+  variant = 'auto',
 })
 
 function ColorMyPencils(color)
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
   color = color or "rose-pine-moon"
   vim.cmd.colorscheme(color)
 
+  -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+  -- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 ColorMyPencils()
