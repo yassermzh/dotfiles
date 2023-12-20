185f8fc1c384bdb8c5ed299d30fa9608a8b3b1d5
Yasser Zadeh <yamoh@spreadgroup.com>
2023-12-20 21:37:30 +0330

    change color

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -5,7 +5,7 @@ require('rose-pine').setup({
 })
 
 function ColorMyPencils(color)
-  color = color or "rose-pine-moon"
+  color = color or "rose-pine"
   vim.cmd.colorscheme(color)
 
   -- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
