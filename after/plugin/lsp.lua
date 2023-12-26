757f95c59cead28b17312e4e79aa6deb0cf0a046
Yasser Zadeh <yamoh@spreadgroup.com>
2023-12-26 18:19:01 +0330

    remap for colemak

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -26,9 +26,9 @@ local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
 cmp.setup({
   mapping = cmp.mapping.preset.insert({
-    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-i>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-e>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-o>'] = cmp.mapping.confirm({ select = true }),
     ["<C-Space>"] = cmp.mapping.complete(),
     ['<Tab>'] = nil,
     ['<S-Tab>'] = nil,
--- lua/yasser/remap.lua
+++ lua/yasser/remap.lua
@@ -1,14 +1,16 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- move lines
+vim.keymap.set("v", "E", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "I", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set("n", "k", "nzzzv")
+vim.keymap.set("n", "m", "Nzzzv")
+vim.keymap.set("n", "<C-m>", "m")
 
 vim.keymap.set("n", "<leader>vwm", function()
   require("vim-with-me").StartVimWithMe()
@@ -32,13 +34,13 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
 
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "<C-e>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-i>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>e", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>i", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
+-- vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- vim.keymap.set("n", "<leader><leader>", function()
 --     vim.cmd("so")
@@ -50,3 +52,17 @@ vim.keymap.set("n", "<leader>w", ':w<CR>')
 
 
 vim.keymap.set("n", "<leader>z", "<cmd>only<CR>")
+
+vim.keymap.set({'n', 'v'}, 'n', 'h', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'e', 'j', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'i', 'k', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'o', 'l', { noremap = true })
+vim.keymap.set({'n', 'v'}, 't', 'i', { noremap = true })
+vim.keymap.set({'n', 'v'}, 'T', 'I', { noremap = true })
+vim.keymap.set('n', 'l', 'o', { noremap = true })
+vim.keymap.set('n', 'L', 'O', { noremap = true })
+-- vim.keymap.set('n', 'h', ':', { noremap = true })
+vim.keymap.set('n', '<C-,>', '<C-o>', { noremap = true })
+vim.keymap.set('n', '<C-.>', '<C-i>', { noremap = true })
+vim.keymap.set('n', 'E', 'e', { noremap = true })
+vim.keymap.set('i', '<C-x>', '<Esc>lxi', { noremap = true })
