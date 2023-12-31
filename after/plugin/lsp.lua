a8970b4cab5ce38625b86d5131c51607d0f47168
Yasser Zadeh <yamoh@spreadgroup.com>
2023-12-31 17:00:21 +0100

    ignore type definitions when doing go-to-definition

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -96,9 +96,41 @@ local function organize_imports()
   vim.lsp.buf.execute_command(params)
 end
 
+local function filter(arr, fn)
+  if type(arr) ~= "table" then
+    return arr
+  end
+
+  local filtered = {}
+  for k, v in pairs(arr) do
+    if fn(v, k, arr) then
+      table.insert(filtered, v)
+    end
+  end
+
+  return filtered
+end
+
+local function filterDTS(value)
+  print('value', value)
+  return string.match(value.targetUri, 'd.ts') == nil
+end
+
+
 require("lspconfig").tsserver.setup {
   -- on_attach = on_attach,
   -- capabilities = capabilities,
+  handlers = {
+    -- https://github.com/typescript-language-server/typescript-language-server/issues/216
+    ['textDocument/definition'] = function(err, result, method, ...)
+      if vim.tbl_islist(result) and #result > 1 then
+        local filtered_result = filter(result, filterDTS)
+        return vim.lsp.handlers['textDocument/definition'](err, filtered_result, method, ...)
+      end
+
+      vim.lsp.handlers['textDocument/definition'](err, result, method, ...)
+    end
+  },
   commands = {
     OrganizeImports = {
       organize_imports,
