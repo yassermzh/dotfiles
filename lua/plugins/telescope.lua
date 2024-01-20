a857fc0ebfaccf580c087802770c5e058aa5ab26
Yasser Zadeh <yamoh@spreadgroup.com>
2024-01-20 22:18:44 +0100

    telescope cleanup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,12 +11,7 @@ return {
     config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>pb', builtin.find_files, {})
       vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-      -- vim.keymap.set('n', '<leader>ps', function()
-      -- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
-      -- end)
-
       vim.keymap.set('n', '<leader>ps', builtin.grep_string)
       -- vim.keymap.set('n', '<leader>pS', builtin.live_grep)
 
