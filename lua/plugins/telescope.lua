f48282500696bc94511703e7e90a3ad85b981ec3
Yasser Zadeh <yamoh@spreadgroup.com>
2024-01-20 22:19:48 +0100

    enhance telescope live grep

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,18 +12,29 @@ return {
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
       vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-      vim.keymap.set('n', '<leader>ps', builtin.grep_string)
-      -- vim.keymap.set('n', '<leader>pS', builtin.live_grep)
-
+      -- vim.keymap.set('n', '<leader>ps', builtin.grep_string)
       vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
       -- vim.keymap.set("n", "<leader><leader>", "<Cmd>Telescope frecency<CR>")
-      vim.keymap.set("n", "<leader>pS", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
       local telescope = require("telescope")
+      local lga_actions = require("telescope-live-grep-args.actions")
+      local live_grep_args_shortcuts = require("telescope-live-grep-args.shortcuts")
+
+      vim.keymap.set("n", "<leader>ps", live_grep_args_shortcuts.grep_word_under_cursor)
+      vim.keymap.set("n", "<leader>pS", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
 
       telescope.setup({
         extensions = {
+          live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            mappings = {         -- extend mappings
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+                ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+              },
+            },
+          },
           media_files = {
             -- filetypes whitelist
             -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
