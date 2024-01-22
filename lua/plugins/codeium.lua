df759f983634c13e4a53244742543d659a523ed0
Yasser Zadeh <yamoh@spreadgroup.com>
2024-01-22 13:11:15 +0100

    enable codeium

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -2,7 +2,7 @@ return {
   {
     "Exafunction/codeium.vim",
     config = function()
-      vim.g.codeium_manual = true
+      vim.g.codeium_manual = false
       vim.keymap.set('i', '<C-\\>', function() return vim.fn['codeium#Complete']() end, { expr = true })
     end
   }
