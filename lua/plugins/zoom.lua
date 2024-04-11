b4f77339fb0382d88f7c696101030c2ed7a56e98
Yasser Zadeh <yamoh@spreadgroup.com>
2024-04-11 16:32:33 +0200

    fix zoom keymap

--- lua/plugins/zoom.lua
+++ lua/plugins/zoom.lua
@@ -2,11 +2,9 @@ return {
   {
     "anuvyklack/windows.nvim",
     dependencies = "anuvyklack/middleclass",
-    keys = {
-      '<C-w>z', "<cmd>WindowsMaximize<CR>", desc = "Maximize window"
-    },
     config = function()
       require('windows').setup()
+      vim.keymap.set("n", "<C-w>z", "<cmd>WindowsMaximize<CR>", { desc = "Maximize window" })
     end
   }
 }
