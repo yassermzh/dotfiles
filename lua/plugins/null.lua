9069eb1e538c4fecbcacd5d04d1d676cb991cd4c
Yasser Zadeh <yamoh@spreadgroup.com>
2024-07-06 19:51:56 +0200

    fixup! update eslint null stuff

--- lua/plugins/null.lua
+++ lua/plugins/null.lua
@@ -27,6 +27,17 @@ return {
             },
             command = "eslint_d",
           }),
+          require("none-ls.code_actions.eslint").with({
+            name = "eslint_d",
+            meta = {
+              url = "https://github.com/mantoni/eslint_d.js/",
+              description = "Like ESLint, but faster.",
+              notes = {
+                "Once spawned, the server will continue to run in the background. This is normal and not related to null-ls. You can stop it by running `eslint_d stop` from the command line.",
+              },
+            },
+            command = "eslint_d",
+          }),
           null_ls.builtins.diagnostics.selene,
         },
         on_attach = function(client, bufnr)
