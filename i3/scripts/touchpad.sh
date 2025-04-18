69be3919d1e15980f844ed7fcade5959ed9e16b8
yassermzh <yassermzh@gmail.com>
2025-04-18 12:46:02 +0200

    trackpoint

--- i3/scripts/touchpad.sh
+++ i3/scripts/touchpad.sh
@@ -1,11 +1,17 @@
 #! /bin/bash
 
-read TPdevice <<< $( xinput | sed -nre '/TouchPad|Touchpad/s/.*id=([0-9]*).*/\1/p' )
-state=$( xinput list-props "$TPdevice" | grep "Device Enabled" | grep -o "[01]$" )
+read TOUCHPAD_ID <<< $( xinput | sed -nre '/TouchPad|Touchpad/s/.*id=([0-9]*).*/\1/p' )
+state=$( xinput list-props "$TOUCHPAD_ID" | grep "Device Enabled" | grep -o "[01]$" )
 
+# toggle touchpad
 if [ "$state" -eq '1' ];then
-  xinput --disable "$TPdevice" && notify-send -i emblem-nowrite "Touchpad" "Disabled"
+  xinput --disable "$TOUCHPAD_ID" && notify-send -i emblem-nowrite "Touchpad" "Disabled"
 else
-  xinput --enable "$TPdevice" && notify-send -i emblem-nowrite "Touchpad" "Enabled"
+  xinput --enable "$TOUCHPAD_ID" && notify-send -i emblem-nowrite "Touchpad" "Enabled"
+  xinput set-prop "$TOUCHPAD_ID" "libinput Accel Speed" 0.4
 fi
+
+TRACKPOINT_ID=$(xinput list | grep -i trackpoint | sed -n 's/.*id=\([0-9]*\).*/\1/p')
+xinput set-prop "$TRACKPOINT_ID" "libinput Accel Speed" -0.4
+
 
