5488e154644d01f532aab9576b7c86cf965f821d
yassermzh <yassermzh@gmail.com>
2025-07-09 13:15:49 +0200

    disable noice listing as buffer

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -1,29 +1,56 @@
 return {
   {
-  "folke/noice.nvim",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
-    "nvim-lua/plenary.nvim",
-    "rcarriga/nvim-notify"
-  },
-  config = function()
-    require("noice").setup({
-      cmdline = {
-        view = "cmdline_popup", -- Use floating popup for command line
-      },
-      views = {
-        cmdline_popup = {
-          position = {
-            row = "50%", -- Middle of screen
-            col = "50%",
-          },
-          size = {
-            width = 60,
-            height = "auto",
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "rcarriga/nvim-notify"
+    },
+    config = function()
+      require("noice").setup({
+        cmdline = {
+          view = "cmdline_popup", -- Use floating popup for command line
+        },
+        views = {
+          cmdline_popup = {
+            position = {
+              row = "50%", -- Middle of screen
+              col = "50%",
+            },
+            size = {
+              width = 60,
+              height = "auto",
+            },
           },
         },
-      },
-    })
-  end,
+        -- 👇 Add this to prevent long messages going to splits
+        presets = {
+          long_message_to_split = false,
+        },
+        -- 👇 Optional: mark Noice buffers as unlisted
+        routes = {
+          {
+            filter = {
+              event = "msg_show",
+              any = {
+                { kind = "confirm" },
+                { kind = "confirm_sub" },
+                { kind = "" }, -- normal messages
+              },
+            },
+            opts = { skip = false },
+          },
+        },
+      })
+
+      -- 👇 Extra protection: mark any Noice buffer as unlisted
+      vim.api.nvim_create_autocmd("BufWinEnter", {
+        pattern = "noice://*",
+        callback = function()
+          vim.opt_local.buflisted = false
+        end,
+      })
+    end,
+  }
 }
-}
+
