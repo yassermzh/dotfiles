d279f0f2d63546ff9be89970c101be3d146d229d
yassermzh <yassermzh@gmail.com>
2025-07-09 13:17:18 +0200

    update codium

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,5 +1,6 @@
 return {
-  "Exafunction/codeium.nvim",
+  "Exafunction/windsurf.nvim",
+  event = 'BufEnter',
   dependencies = {
     "nvim-lua/plenary.nvim",
     "hrsh7th/nvim-cmp",
@@ -7,7 +8,20 @@ return {
   config = function()
     require("codeium").setup({
     })
-    vim.g.codeium_manual = true
-    vim.keymap.set('i', '<C-\\>', function() return vim.fn['codeium#Complete']() end, { expr = true })
+    -- vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Complete']() end, { expr = true })
+
+    -- Change '<C-g>' here to any keycode you like.
+    -- vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true })
+    -- vim.keymap.set('i', '<C-,>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+    --   { expr = true, silent = true })
+    -- vim.keymap.set('i', '<C-.>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+    --   { expr = true, silent = true })
+    -- vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end, { expr = true, silent = true })
+
+
+    -- These use the new Lua API and support multiline completions
+    vim.keymap.set('i', '<C-\\>', function() return vim.fn['codeium#Accept']() end, { expr = true, silent = true, desc = "Accept Codeium suggestion" })
+
+    -- vim.g.codeium_manual = true
   end
 }
