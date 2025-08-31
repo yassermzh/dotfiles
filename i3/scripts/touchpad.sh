30fe4b4fa42680ced21ead6f18f061926a883cdf
yassermzh <yassermzh@gmail.com>
2025-08-31 11:39:03 +0200

    do not disable touchpad

--- i3/scripts/touchpad.sh
+++ i3/scripts/touchpad.sh
@@ -5,7 +5,7 @@ state=$( xinput list-props "$TOUCHPAD_ID" | grep "Device Enabled" | grep -o "[01
 
 # toggle touchpad
 if [ "$state" -eq '1' ];then
-  xinput --disable "$TOUCHPAD_ID" && notify-send -i emblem-nowrite "Touchpad" "Disabled"
+  xinput set-prop "$TOUCHPAD_ID" "libinput Accel Speed" 0.7
 else
   xinput --enable "$TOUCHPAD_ID" && notify-send -i emblem-nowrite "Touchpad" "Enabled"
   xinput set-prop "$TOUCHPAD_ID" "libinput Accel Speed" 0.7
